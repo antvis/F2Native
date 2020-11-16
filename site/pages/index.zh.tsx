d8a4f23f4087375f0d94bd06d15222297996e305
JaneZhong <z123j@sina.com>
2020-11-16 20:44:54 +0800

    fix:[homepage update]

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -16,7 +16,7 @@ const IndexPage = () => {
         'https://gw.alipayobjects.com/zos/basement_prod/5dbaf094-c064-4a0d-9968-76020b9f1510.svg',
       title: t('专注客户端，极致体验'),
       description: t(
-        '围绕设计、性能以及异构环境，为用户提供移动客户端图表的最佳实践',
+        '围绕设计、性能以及不同平台环境，为用户提供移动客户端图表的最佳实践',
       ),
     },
     {
@@ -24,15 +24,15 @@ const IndexPage = () => {
         'https://gw.alipayobjects.com/zos/basement_prod/0a0371ab-6bed-41ad-a99b-87a5044ba11b.svg',
       title: t('图表丰富，组件完备'),
       description: t(
-        '基于图形语法，可灵活构建各类图表（50+），组件完备，覆盖各类场景',
+        '基于图形语法，灵活构建各类图表；组件完备，覆盖各类场景',
       ),
     },
     {
       icon:
         'https://gw.alipayobjects.com/zos/basement_prod/716d0bc0-e311-4b28-b79f-afdd16e8148e.svg',
-      title: t('扩展灵活，创意无限'),
+      title: t('轻量化植入，自然呈现'),
       description: t(
-        '插件机制，图形、交互均可灵活扩展，使用更自由',
+        '设计上自然简单易懂,可视化展现和人的自然行为特征有机融合;引入灵活、学习成本低',
       ),
     },
   ];
@@ -46,13 +46,13 @@ const IndexPage = () => {
     {
       logo: 'https://antv.alipay.com/assets/image/home/f2/caifu-logo.png',
       title: t('蚂蚁财富'),
-      description: t('已广泛应用于黄金、股票等各个金融业务场景中，支撑着蚂蚁财富 app 上众多可视化场景。同时通过深入剖析用户的可视化诉求，沉淀出多套面向金融的可视化方案。'),
+      description: t('已广泛应用于股票等金融业务场景中，支撑着蚂蚁财富 app 上众多可视化场景。同时通过深入剖析用户的可视化诉求，沉淀出多套面向金融的可视化方案。'),
       image: 'https://gw.alipayobjects.com/mdn/rms_04a9e5/afts/img/A*W8dZTIH9hUAAAAAAAAAAAAAAARQnAQ',
     },
     {
       logo: 'https://antv.alipay.com/assets/image/home/f2/alipay-logo.png',
       title: t('支付宝'),
-      description: t('覆盖证券、黄金、新闻财报等多个场景,在效能提升和高性能体验方面都有着优秀的表现'),
+      description: t('覆盖证券、新闻财报等多个场景,在效能提升和高性能体验等方面都有着优秀的表现'),
       image: 'https://gw.alipayobjects.com/mdn/rms_04a9e5/afts/img/A*x8WhQZAUfjgAAAAAAAAAAAAAARQnAQ',
     },
   ];
@@ -65,7 +65,7 @@ const IndexPage = () => {
     },
     {
       text: t('图表演示'),
-      link: `/${i18n.language}/docs/examples/line/line`,
+      link: `/${i18n.language}/docs/examples/`,
     },
   ];
 
@@ -83,7 +83,7 @@ const IndexPage = () => {
         }
         title={t('F2Native 移动端可视化方案')}
         description={t(
-          'F2Native 是一个专注于客户端，开箱即用的高性能可视化解决方案，完备的图形语法理论，满足你的各种可视化需求，专业的移动设计指引为你带来最佳的移动端图表体验。',
+          'F2Native 是一个专注于客户端，开箱即用、高性能的可视化解决方案，完备的图形语法理论，满足你的各种需求，专业的移动设计指引为你带来最佳的移动端图表体验。',
         )}
         className="banner"
         buttons={bannerButtons}

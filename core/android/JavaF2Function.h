69a27994ccb0be65ce6f905ed258f5dc286cbb70
yangxlei <jxyl9010@gmail.com>
2021-06-28 15:34:00 +0800

    fix: check Jni Bridges exception.

--- core/android/JavaF2Function.h
+++ core/android/JavaF2Function.h
@@ -1,3 +1,4 @@
+#include "android/F2NativeJNI.h"
 #include "android/JNIUtil.h"
 #include "graphics/func/Func.h"
 #include <jni.h>
@@ -16,21 +17,37 @@ class JavaF2Function : public func::F2Function {
     JavaF2Function(ScopedJavaGlobalRef<jobject> *_handle) : func::F2Function(), handle_(_handle) {}
 
     nlohmann::json Execute(nlohmann::json t = nlohmann::json()) override {
+        JNIEnv *env = GetJniEnvSafe();
+        if(env == nullptr || handle_ == nullptr || handle_->isNull()) {
+            return nlohmann::json();
+        }
+
+        if(nExecuteMethod_ == nullptr) {
+            jclass functionClass = env->GetObjectClass(handle_->obj());
+            nExecuteMethod_ = env->GetMethodID(functionClass, "nExecute", "(Ljava/lang/String;)Ljava/lang/String;");
+        }
+
+        // double check nExecuteMethod_.
+        if(nExecuteMethod_ == nullptr || jni::HasException(env)) {
+            F2_LOG_E("JavaF2Function", "Get nExecute Method failed.  functionId: %s", functionId.data());
+            jni::ClearException(env);
+            return nlohmann::json();
+        }
+
         std::string param;
         if(t.is_string()) {
             param = t;
         } else {
             param = t.dump();
         }
-        JNIEnv *env = GetJniEnvSafe();
-        if(env == nullptr)
-            return nlohmann::json();
         jstring jparam = StringToJString(env, param);
-        if(nExecuteMethod_ == nullptr) {
-            jclass functionClass = env->GetObjectClass(handle_->obj());
-            nExecuteMethod_ = env->GetMethodID(functionClass, "nExecute", "(Ljava/lang/String;)Ljava/lang/String;");
-        }
         jstring jrst = (jstring)env->CallObjectMethod(handle_->obj(), nExecuteMethod_, jparam);
+
+        if(jni::HasException(env)) {
+            F2_LOG_E("JavaF2Function", "execute nExecute Method failed.  functionId: %s", functionId.data());
+            jni::ClearException(env);
+            return nlohmann::json();
+        }
 
         std::string result = JavaStringToString(env, jrst);
 
@@ -45,10 +62,11 @@ class JavaF2Function : public func::F2Function {
     ~JavaF2Function() override {
         handle_->Reset();
         delete handle_;
+        handle_ = nullptr;
     }
 
   private:
-    ScopedJavaGlobalRef<jobject> *handle_;
+    ScopedJavaGlobalRef<jobject> *handle_ = nullptr;
 };
 } // namespace jni
 } // namespace xg

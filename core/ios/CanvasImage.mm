24deb80a8a3a321d9bb06641dc1855837f242d10
weiqing.twq <weiqing.twq@antgroup.com>
2022-06-17 16:21:18 +0800

    feat: ios image downloader

--- core/ios/CanvasImage.mm
+++ core/ios/CanvasImage.mm
@@ -12,19 +12,17 @@
 #include <UIKit/UIKit.h>
 #include <CoreGraphics/CoreGraphics.h>
 
-using namespace xg;
-using namespace xg::canvas;
+typedef void (^F2OnLoadCallback)(UIImage *_Nonnull image);
 
-CanvasImage::~CanvasImage() {
-    if (image_) {
-        CGImageRelease(CGImageRef(image_));
-        image_ = nil;
-    }
-}
+@interface F2CanvasImageProxy : NSObject
 
-void CanvasImage::OnLoad(std::function<void()> finished) {
+@end
+
+@implementation F2CanvasImageProxy
+
+-(void)onLoad:(NSString *)urlStr callback:(F2OnLoadCallback) callback {
     //todo 在支付宝域内可借助域内的图片下载能力 提供更好的体验
-    NSURL *url = [NSURL URLWithString:[NSString stringWithUTF8String:url_.c_str()]];
+    NSURL *url = [NSURL URLWithString:urlStr];
     
     dispatch_queue_t asynchronousQueue = dispatch_queue_create("imageDownloadQueue", NULL);
     //创建异步线程
@@ -36,14 +34,41 @@ void CanvasImage::OnLoad(std::function<void()> finished) {
         
         //回到主线程更新UI 已经释放了锁
         //TODO 判断this对象是否被释放了
-        if(!error && image) {
+        if(!error && image && callback) {
             dispatch_async(dispatch_get_main_queue(), ^{
-                image_ = image.CGImage;
-                CGImageRetain(CGImageRef(image_));
-                finished();
+                callback(image);
             });
         }
     });
+}
+
+@end
+
+using namespace xg;
+using namespace xg::canvas;
+
+CanvasImage::~CanvasImage() {
+    if (image_) {
+        CGImageRelease(CGImageRef(image_));
+        image_ = nil;
+    }
+    
+    F2CanvasImageProxy *proxy = (__bridge_transfer F2CanvasImageProxy *)downloader_;
+    proxy = nullptr;
+}
+
+void CanvasImage::OnLoad(std::function<void()> finished) {
+    if (!downloader_) {
+        downloader_ = (__bridge_retained void *)[[F2CanvasImageProxy alloc] init];
+    }
+    
+    F2CanvasImageProxy *proxy = (__bridge F2CanvasImageProxy *)downloader_;
+    
+    [proxy onLoad:[NSString stringWithUTF8String:url_.c_str()] callback:^(UIImage * _Nonnull image) {
+        image_ = image.CGImage;
+        CGImageRetain(CGImageRef(image_));
+        if(finished) finished();
+    }];
 }
 
 float CanvasImage::GetWidth() {

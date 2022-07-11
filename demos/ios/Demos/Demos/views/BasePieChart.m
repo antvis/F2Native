d872d8c497fadb2e93e49a0c7d082b0f47259087
weiqing.twq <weiqing.twq@antgroup.com>
2022-07-11 14:27:51 +0800

    rm:unused config for timesharinglinear

--- demos/ios/Demos/Demos/views/BasePieChart.m
+++ demos/ios/Demos/Demos/views/BasePieChart.m
@@ -7,11 +7,12 @@
 - (void)chartRender {
     NSString *jsonPath = [[NSBundle mainBundle] pathForResource:@"Res/mockData_basePie" ofType:@"json"];
     NSString *jsonData = [NSString stringWithContentsOfFile:jsonPath encoding:NSUTF8StringEncoding error:nil];
-    self.chart.canvas(self.canvasView).padding(20, 20, 20, 0.f).source(jsonData);
+    self.chart.canvas(self.canvasView).padding(20, 20, 20, 0.f);
+    self.chart.source([F2Utils toJsonArray:jsonData]);
     self.chart.axis(@"percent", @{@"line": @(NO), @"label": @(NO), @"grid": @(NO)});
     self.chart.axis(@"a", @{@"line": @(NO), @"label": @(NO) , @"grid": @(NO)});
     self.chart.coord(@{@"type": @"polar", @"transposed": @(YES)});
-    self.chart.interval().style(@{@"lineWidth": @(1),@"stroke": @"#ffffff"}).position(@"a*percent").color(@"name", @[]).adjust(@"stack").fixedSize(1);
+    self.chart.interval().position(@"a*percent").color(@"name", @[]).adjust(@"stack").fixedSize(1).style(@{@"lineWidth": @(1),@"stroke": @"#ffffff"});
     self.chart.animate(@YES);
     self.chart.render();
 }

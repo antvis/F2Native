bd38cff1f262dbd6736605fdc368e865913835f0
weiqing.twq <weiqing.twq@antgroup.com>
2022-08-24 18:23:17 +0800

    feat:update build sh

--- wasi/build.sh
+++ wasi/build.sh
@@ -1,5 +1,4 @@
-rm -rf build
-mkdir build
-cd build
-cmake .. -G Ninja
-ninja
+#for WASI
+rm -rf build && mkdir build && cd build
+emcmake cmake  -DEMSCRIPTEN_FORCE_COMPILERS=OFF .. -DCMAKE_CXX_FLAGS="-DNDEBUG"
+emmake make 
